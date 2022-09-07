e0e8ba34609a094f8cdaf141252593e2814f4f81
geomarian <geomarian520@gmail.com>
2022-09-08 00:14:45 +0530

    Snake and ladder UC2 added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -18,6 +18,10 @@ public class SnakeAndLadder {
 		// TODO Auto-generated method stub
 		int startPosition = 0;
 		System.out.println("Player position is: " + startPosition);
+		Random random = new Random();
+		int dice = 1 + random.nextInt(6);
+		System.out.println("Dice Rolling...");
+		System.out.println("Dice Number: " + dice);
 	}
 
 }
