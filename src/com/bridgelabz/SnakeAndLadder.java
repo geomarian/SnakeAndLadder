3466900923d78953ef9cd119f56772b0b36a0426
geomarian <geomarian520@gmail.com>
2022-09-08 00:35:07 +0530

    UC3 refractored

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -17,23 +17,26 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int startPosition = 0;
-		System.out.println("Player position is: " + startPosition);
+	
+		
+		//System.out.println("Player position is: " + startPosition);
 		Random random = new Random();
 		int dice = 1 + random.nextInt(6);
 		System.out.println("Dice Rolling...");
 		System.out.println("Dice Number: " + dice);
-		// To Get Random Number For No Play, Ladder, Snake.
-		int playCheck = random.nextInt();
+		
+		int playCheck = random.nextInt(7);
 		
 		// To Check Options To No Play, Ladder Or Snake
 		switch (playCheck) {
 		case 0:
-			System.out.println(" No Play");
+			System.out.println("No Play");
 			break;
+			
 		case 1:
-			System.out.println(" Ladder");
+			System.out.println("Ladder");
 			startPosition = startPosition + dice;
-			break;
+				break;
 			
 		default:
 			System.out.println("Snake");
