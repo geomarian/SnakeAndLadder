af8d0e4197bb066254d67556cfcb7886db192d8f
geomarian <geomarian520@gmail.com>
2022-09-08 00:09:53 +0530

    Added UC1 snake and ladder

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+import java.util.Random;
+
+/**
+ * @author ASUS
+ *
+ */
+public class SnakeAndLadder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int startPosition = 0;
+		System.out.println("Player position is: " + startPosition);
+	}
+
+}
