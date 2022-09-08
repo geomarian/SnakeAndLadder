6dfe6dad6704c6a186823415c57c5f9661fb2406
geomarian <geomarian520@gmail.com>
2022-09-08 15:06:36 +0530

    UC 4 updated to Repeat till the Player position.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,6 +4,8 @@
 package com.bridgelabz;
 
 import java.util.Random;
+import java.util.Scanner;
+
 
 /**
  * @author ASUS
@@ -16,40 +18,49 @@ public class SnakeAndLadder {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int startPosition = 0;
-		int winPosition = 100;
-		
-		//System.out.println("Player position is: " + startPosition);
-		Random random = new Random();
-		int dice = 1 + random.nextInt(6);
-		System.out.println("Dice Rolling...");
-		System.out.println("Dice Number: " + dice);
-		
-		int playCheck = random.nextInt(3);
-		//int playCheck =0;
-		// To Check Options To No Play, Ladder Or Snake
-		switch (playCheck) {
-		case 0:
-			System.out.println("No Play");
-			break;
-			
-		case 1:
-			System.out.println("Ladder");
-			startPosition = startPosition + dice;
-			if(startPosition > winPosition) {
-				startPosition = startPosition - dice;
-			}
-				break;
-			
-		default:
-			System.out.println("Snake");
-			startPosition = startPosition - dice;
-			if(startPosition <0) {
-				startPosition = 0;
-			}
-			break;
-		}
-	System.out.println("The position is: " + startPosition);
-	}
 
-}
+		int winPosition = 100;
+
+		Scanner input = new Scanner(System.in);
+	
+		for(int startPositionP1 = 0; startPositionP1 != winPosition;) {
+			//startGame
+			System.out.println("Press 1 to roll dice: ");
+
+			int num = input.nextInt();
+
+			if(num==1) {
+
+				Random random = new Random();
+				int dice = 1 + random.nextInt(6);
+				System.out.println("Dice rolled and number is : " + dice);
+
+				
+				int playCheck = random.nextInt(3);
+
+		
+				switch (playCheck) {
+				case 0:
+					System.out.println("No Play");
+					break;
+
+				case 1:
+					System.out.println("Ladder");
+					startPositionP1 = startPositionP1 + dice;
+					if(startPositionP1 > winPosition) {
+						startPositionP1 = startPositionP1 - dice;
+					}
+					break;
+
+				default:
+					System.out.println("Snake");
+					startPositionP1 = startPositionP1 - dice;
+					startPositionP1 = 0;
+
+					break;
+				}
+				System.out.println("The position is: " + startPositionP1);
+			}
+
+		}
+	}}
