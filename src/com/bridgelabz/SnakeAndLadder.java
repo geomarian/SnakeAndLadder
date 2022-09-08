055302e46db0c934e44cfaab92a1287d6e638104
geomarian <geomarian520@gmail.com>
2022-09-08 17:51:01 +0530

    UC 7 refractored

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -18,20 +18,20 @@ public class SnakeAndLadder {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int winPosition = 100;
 		int diceRolled = 0;
 		int numberOfPlayer = 2;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Number Of Player: " + numberOfPlayer);
-		
+
 		Random rand = new Random();
 		int dice = 1 + rand.nextInt(6);
-		
+
 		//startGame Player 1 
 		for(int startPositionP1 = 0; startPositionP1 != winPosition;) {
-			
-			
+
+
 			//startGame Player 2 
 			for(int startPositionP2 = 0; startPositionP2 != winPosition;) {
 				System.out.println("Press 2 to roll dice: ");
@@ -41,42 +41,42 @@ public class SnakeAndLadder {
 				if(num2==2) {
 
 					Random random = new Random();
-			//		int dice = 1 + random.nextInt(6);
+					//		int dice = 1 + random.nextInt(6);
 					System.out.println("-----------------");
 					System.out.println("Dice rolled and number is : " + dice);
 					int playCheck = random.nextInt(3);
 					String str1 = "Ladder";
 					if(playCheck==0) {
-						
+
 						System.out.println("No Play");
 					}
-//					 if(playCheck==1) {
-//						 if(str1!=null) {
-//								System.out.println("Got Ladder - Player 1 will play again!");
-//						 	
-//					}
-					
+					//					 if(playCheck==1) {
+					//						 if(str1!=null) {
+					//								System.out.println("Got Ladder - Player 1 will play again!");
+					//						 	
+					//					}
+
 					while(str1 != "Ladder") {
 						System.out.println("Got Ladder - Player 1 will play again!");
-				 	 str1 = input.nextLine();
-						
+						str1 = input.nextLine();
+
 					}
-						System.out.println("Ladder - Player stepped Up");
-						startPositionP2 = startPositionP2 + dice;
-					}
-						
-						
-					else{
-						System.out.println("Snake - Player stepped Down");
-						startPositionP2 = startPositionP2 - dice;
-						startPositionP2 = 0;
-					}
-					System.out.println("The position is: " + startPositionP2);
-					System.out.println("-----------------");
+					System.out.println("Ladder - Player stepped Up");
+					startPositionP2 = startPositionP2 + dice;
+				}
+
+
+				else{
+					System.out.println("Snake - Player stepped Down");
+					startPositionP2 = startPositionP2 - dice;
+					startPositionP2 = 0;
+				}
+				System.out.println("The position is: " + startPositionP2);
+				System.out.println("-----------------");
 			}
-				
+
 			//for Player 1
-			
+
 			System.out.println("Press 1 to roll dice: ");
 
 			int num = input.nextInt();
@@ -88,10 +88,10 @@ public class SnakeAndLadder {
 				System.out.println("-----------------");
 				System.out.println("Dice rolled and number is : " + dice);
 
-				
+
 				int playCheck = random.nextInt(3);
 
-		
+
 				switch (playCheck) {
 				case 0:
 					System.out.println("No Play");
@@ -116,11 +116,11 @@ public class SnakeAndLadder {
 				}
 				System.out.println("The position is: " + startPositionP1);
 				System.out.println("-----------------");
-				 
+
 			}	
 		}
 		System.out.println("You Rolled Dices " + diceRolled + " Times.");
 		System.out.println("Congratulations! You Won The Game.");
 	}
-	}
+}
 
